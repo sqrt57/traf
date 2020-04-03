0d0cdb203d786568716a75eb6dd3f9dc998f11fe
Dmitry Grigoryev <dmitriy57@gmail.com>
2020-04-04 00:02:27 +0300

    Parse empty module

--- trafc.Tests/parser.fs
+++ trafc.Tests/parser.fs
@@ -6,5 +6,14 @@ open Swensen.Unquote
 open Triton
 
 [<Fact>]
+let ``Parse empty source`` () =
+    test <@ Ast.TopLevel [||] = Parser.parse []  @>
+
+[<Fact>]
 let ``Parse empty module`` () =
-    test <@ Ast.TopLevel [||] = Parser.parse [||]  @>
+    test <@ Ast.TopLevel [| { name = "X" } |] =
+        Parser.parse [ Lexeme.Identifier "module"
+                       Lexeme.Identifier "X"
+                       Lexeme.LeftCurly
+                       Lexeme.RightCurly ]
+    @>
--- trafc/parser.fs
+++ trafc/parser.fs
@@ -7,13 +7,42 @@ open System.Collections.Generic
 [<RequireQualifiedAccess>]
 module Ast =
 
+    [<RequireQualifiedAccess>]
     type Module = { name: string }
 
     type TopLevel = TopLevel of Module array
 
 module Parser =
 
-    exception ParserError of {| fileName: string; message: string |}
+    exception ParserError of {| message: string |}
 
-    let parse (lexemes: Lexeme array) : Ast.TopLevel =
-        Ast.TopLevel [||]
+    let parse (lexemes: Lexeme list) : Ast.TopLevel =
+        let modules = ResizeArray<Ast.Module>()
+
+        let moduleBody lexemes =
+            match lexemes with
+            | [] -> raise <| ParserError {| message = sprintf "expected opening curly bracket after module name but got EOF" |}
+            | Lexeme.LeftCurly :: rest ->
+                match rest with
+                | [] -> raise <| ParserError {| message = sprintf "expected closing curly bracket after module body but got EOF" |}
+                | Lexeme.RightCurly :: rest -> rest
+                | l :: _ -> raise <| ParserError {| message = sprintf "expected closing curly bracket after module body but got %A" l |}
+            | l :: _ -> raise <| ParserError {| message = sprintf "expected opening curly bracket after module name but got %A" l |}
+
+
+        let rec topLevel lexemes =
+            match lexemes with
+            | [] -> ()
+            | Lexeme.Identifier "module" :: rest ->
+                match rest with
+                | Lexeme.Identifier name :: rest ->
+                    let rest = moduleBody rest
+                    { Ast.Module.name = name } |> modules.Add
+                    topLevel rest
+                | [] -> ()
+                | l :: _ -> raise <| ParserError {| message = sprintf "expected module name after module keyword but got %A" l |}
+            | l :: _ -> raise <| ParserError {| message = sprintf "unexpected lexeme at top level: %A" l |}
+
+        topLevel lexemes
+
+        Ast.TopLevel (modules.ToArray())
