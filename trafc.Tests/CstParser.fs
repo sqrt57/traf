354a2038db121626ad26b3d95b68af8e0ea78d2e
Dmitry Grigoryev <dmitriy57@gmail.com>
2020-06-29 17:03:43 +0300

    Parse constant definition stub

--- trafc.Tests/CstParser.fs
+++ trafc.Tests/CstParser.fs
@@ -11,9 +11,42 @@ let ``Parse empty source`` () =
 
 [<Fact>]
 let ``Parse empty module`` () =
-    test <@ Cst.TopLevel [ { name = "X"; definitions = [] } ] =
-        CstParser.parse [ Lexeme.Identifier "module"
-                          Lexeme.Identifier "X"
-                          Lexeme.LeftCurly
-                          Lexeme.RightCurly ]
-    @>
+    let expected = Cst.TopLevel [ { name = "X"; definitions = [] } ]
+    let actual = CstParser.parse [ Lexeme.Identifier "module"
+                                   Lexeme.Identifier "X"
+                                   Lexeme.LeftCurly
+                                   Lexeme.RightCurly ]
+    test <@ expected = actual @>
+
+[<Fact>]
+let ``Parse two empty modules`` () =
+    let expected = Cst.TopLevel [ { name = "X"; definitions = [] }
+                                  { name = "Y"; definitions = [] } ]
+    let actual = CstParser.parse [ Lexeme.Identifier "module"
+                                   Lexeme.Identifier "X"
+                                   Lexeme.LeftCurly
+                                   Lexeme.RightCurly
+                                   Lexeme.Identifier "module"
+                                   Lexeme.Identifier "Y"
+                                   Lexeme.LeftCurly
+                                   Lexeme.RightCurly ]
+    test <@ expected = actual @>
+
+
+[<Fact>]
+let ``Parse const definition stub`` () =
+    let definitions = [ Cst.ConstDefinition { name = "C"; type_ = Cst.Builtin "int32"; value = Cst.IntVal 15L } ]
+    let expected = Cst.TopLevel [ { name = "Mod"; definitions = definitions } ]
+    let actual = CstParser.parse [ Lexeme.Identifier "module"
+                                   Lexeme.Identifier "Mod"
+                                   Lexeme.LeftCurly
+                                   Lexeme.Identifier "const"
+                                   Lexeme.Identifier "C"
+                                   Lexeme.Operator ":"
+                                   Lexeme.Identifier "int32"
+                                   Lexeme.Operator ":="
+                                   Lexeme.Int 15L
+                                   Lexeme.Semicolon
+                                   Lexeme.RightCurly ]
+    test <@ expected = actual @>
+
--- trafc/CstParser.fs
+++ trafc/CstParser.fs
@@ -31,6 +31,7 @@ module Cst =
         | Int of int64
     type AttrDefinition = { name: string; value: AttrValue }
     type AttrList = AttrList of AttrDefinition list
+    type AttrLists = AttrLists of AttrList list
 
     type ConstantDefinition = { name: string; type_: Type; value: Expr; }
     type VariableDefinition = { name: string; type_: Type; value: Expr option; }
@@ -44,8 +45,8 @@ module Cst =
 
     type ModuleTopLevel =
         | ConstDefinition of ConstantDefinition
-        | FunDefinition of {| name: string; type_: FunType; body: FunBody; attributes: AttrList |}
-        | ExternFunDefinition of {| name: string; type_: FunType; attributes: AttrList |}
+        | FunDefinition of {| name: string; type_: FunType; body: FunBody; attributes: AttrLists |}
+        | ExternFunDefinition of {| name: string; type_: FunType; attributes: AttrLists |}
 
     type Module = { name: string; definitions: ModuleTopLevel list }
 
@@ -56,32 +57,79 @@ module CstParser =
     exception CstParserError of {| message: string |}
 
     let parse (lexemes: Lexeme list) : Cst.TopLevel =
-        let modules = ResizeArray<Cst.Module>()
+
+        let expectedButGot expected lexemes =
+            match lexemes with 
+            | lexeme :: _ -> raise <| CstParserError {| message = sprintf "expected %s but got %A" expected lexeme |}
+            | [] -> raise <| CstParserError {| message = sprintf "expected %s but got EOF" expected |}
+
+        let constDefinition lexemes =
+            match lexemes with
+            | Lexeme.Identifier name :: rest ->
+                match rest with
+                | Lexeme.Operator ":" :: rest ->
+                    match rest with
+                    | Lexeme.Identifier typeName :: rest ->
+                        match rest with
+                        | Lexeme.Operator ":=" :: rest ->
+                            match rest with
+                            | Lexeme.Int intValue :: rest ->
+                                match rest with
+                                | Lexeme.Semicolon :: rest ->
+                                    let constantDefinition =
+                                        { Cst.ConstantDefinition.name = name
+                                          Cst.ConstantDefinition.type_ = Cst.Builtin typeName
+                                          Cst.ConstantDefinition.value = Cst.IntVal intValue }
+                                    constantDefinition, rest
+                                | other -> expectedButGot "semicolon after constant definition" other
+                            | other -> expectedButGot "constant value after assignment operator in constant definition" other
+                        | other -> expectedButGot "assignment operator after constant type in constant definition" other
+                    | other -> expectedButGot "constant type after colon in constant definition" other
+                | other -> expectedButGot "colon after constant name in constant definition" other
+            | other -> expectedButGot "constant name after const keyword" other
 
         let moduleBody lexemes =
-            match lexemes with
-            | [] -> raise <| CstParserError {| message = sprintf "expected opening curly bracket after module name but got EOF" |}
-            | Lexeme.LeftCurly :: rest ->
-                match rest with
-                | [] -> raise <| CstParserError {| message = sprintf "expected closing curly bracket after module body but got EOF" |}
-                | Lexeme.RightCurly :: rest -> rest
-                | l :: _ -> raise <| CstParserError {| message = sprintf "expected closing curly bracket after module body but got %A" l |}
-            | l :: _ -> raise <| CstParserError {| message = sprintf "expected opening curly bracket after module name but got %A" l |}
 
+            let moduleBodyItems = ResizeArray<Cst.ModuleTopLevel>()
 
-        let rec topLevel lexemes =
+            let rec moduleBodyItem lexemes =
+                match lexemes with
+                | Lexeme.Identifier "const" :: rest ->
+                    let definition, rest = constDefinition rest
+                    moduleBodyItems.Add <| Cst.ConstDefinition definition
+                    moduleBodyItem rest
+                | Lexeme.RightCurly :: _ -> lexemes
+                | other -> expectedButGot "closing curly bracket after module body" other
+
+            let rest = moduleBodyItem lexemes
+            List.ofSeq moduleBodyItems, rest
+
+        let moduleDefinition lexemes =
             match lexemes with
-            | [] -> ()
-            | Lexeme.Identifier "module" :: rest ->
+            | Lexeme.Identifier name :: rest ->
                 match rest with
-                | Lexeme.Identifier name :: rest ->
-                    let rest = moduleBody rest
-                    { Cst.Module.name = name; Cst.definitions = [] } |> modules.Add
-                    topLevel rest
+                | Lexeme.LeftCurly :: rest ->
+                    let definitions, rest = moduleBody rest
+                    match rest with
+                    | Lexeme.RightCurly :: rest -> { Cst.Module.name = name; Cst.definitions = definitions }, rest
+                    | other -> expectedButGot "closing curly bracket after module body" other
+                | other -> expectedButGot "opening curly bracket after module name" other
+            | other -> expectedButGot "module name after module keyword" other
+
+        let topLevel lexemes =
+
+            let modules = ResizeArray<Cst.Module>()
+
+            let rec topLevelItem lexemes =
+                match lexemes with
                 | [] -> ()
-                | l :: _ -> raise <| CstParserError {| message = sprintf "expected module name after module keyword but got %A" l |}
-            | l :: _ -> raise <| CstParserError {| message = sprintf "unexpected lexeme at top level: %A" l |}
+                | Lexeme.Identifier "module" :: rest ->
+                    let module_, rest = moduleDefinition rest
+                    modules.Add module_
+                    topLevelItem rest
+                | l :: _ -> raise <| CstParserError {| message = sprintf "unexpected lexeme at top level: %A" l |}
+
+            topLevelItem lexemes
+            Cst.TopLevel <| List.ofSeq modules
 
         topLevel lexemes
-
-        Cst.TopLevel <| List.ofSeq modules
